2c8978b86e434dfc1c69a8b336edf28724ad39ec
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-08-13 21:59:32 -0700

    added the mainmenu scene, gameplay scene, and difficulty select scene setup the menus as well as the mainment controller other initial project setup

---
+++ Assets/Scripts/Helper/MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    private MainMenuController instance;
+
+    private void Awake() {
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+    public void LoadMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void LoadDifficultySelectScreen(){
+        SceneManager.LoadScene("DifficultySelect");
+    }
+
+    public void LoadEasyLevel(){
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void LoadMedLevel(){
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void LoadHardLevel(){
+        SceneManager.LoadScene("Gameplay");
+    }
+}
